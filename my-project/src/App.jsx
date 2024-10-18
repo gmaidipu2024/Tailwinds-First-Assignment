88782ebd6bf0f47cd224abd477aea59648828b3c
dipu <gtm10322@gmail.com>
2024-10-18 15:07:12 +0600

    upload Navbar

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Navbar from './assets/Components/Navbar'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1> Hello</h1>
+      <Navbar />
     </>
   )
 }
