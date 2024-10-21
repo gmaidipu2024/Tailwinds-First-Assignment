499bbaabbd30aa32fe2114fa8c60b53caa419a26
dipu <gtm10322@gmail.com>
2024-10-21 10:36:35 +0600

    Picture Update

--- my-project/src/assets/Components/Navbar.jsx
+++ my-project/src/assets/Components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar(Props) {
       <div className='bg-[#E4E4E780] p-5 content-center ml-4 mr-4   '>
         {/* Upper Navbar */}
         <div className=' flex justify-between content-center flex-wrap  '>
-          <img className='mb-2 text-center' src="./public/img/logo.png" alt="" />
+          <img className='mb-2 text-center' src="/img/logo.png" alt="" />
 
           <div className=' grid grid-cols-12 md:grid-cols-1 justify-between items-center ' >
             <ul className='flex flex-wrap flex-row gap-4 text-[#71717A] font-semibold '>
@@ -22,8 +22,8 @@ function Navbar(Props) {
             </ul>
           </div>
           <div className='flex flex-wrap items-center gap-4'>
-            <img className='bg-[#FFFFFF] p-4 rounded-full ' src="./public/img/bel.png" alt="" />
-            <img src="./public/img/Rectangle 5412.png" alt="" />
+            <img className='bg-[#FFFFFF] p-4 rounded-full ' src="/img/bel.png" alt="" />
+            <img src="/img/Rectangle 5412.png" alt="" />
             <div>
               <h1>Goutam Adhikari Dipu</h1>
               <h3>gtm10322@gmail.com</h3>
